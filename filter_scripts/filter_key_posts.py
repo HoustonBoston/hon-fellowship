2d31bc1a8e627ee632adf63e77ab1cf4d060cf32
HoustonBoston <myhorsefly12345@gmail.com>
2026-03-07 21:31:17 -0500

    It's now crazy memory efficient bc of generators (from the yield keyword)

--- filter_scripts/filter_key_posts.py
+++ filter_scripts/filter_key_posts.py
@@ -1,14 +1,12 @@
 # Open file
 import json
-import os
 from datetime import datetime, timezone
 from pathlib import Path
 import argparse
-from posixpath import relpath
 
 parser = argparse.ArgumentParser(description="Filter Reddit posts for specific keywords.")
 parser.add_argument("--keywords", "-k", nargs="+", default=["romance scam", "pig butchering"], help="Keywords to filter posts by")
-parser.add_argument("--input", "-i", default="r_CryptoScams2020-2025_posts.jsonl", help="Input file path")
+parser.add_argument("--input", "-i", help="Input file path", required=True)
 args = parser.parse_args()
 
 path = Path(args.input)
@@ -16,8 +14,11 @@ path = Path(args.input)
 # Filter for keywords ("romance scam", "pig butchering")
 keywords = set(args.keywords)
 
-with open(path, "r", encoding="utf-8") as f:
-    data = [json.loads(line) for line in f if line.strip()]
+# Using yield to read file line by line and parse JSON effiently.
+def extract_json_line(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            yield json.loads(line)
 
 def post_matches(post):
     text = " ".join([
@@ -30,25 +31,6 @@ def post_matches(post):
 def format_date(ts):
     return datetime.fromtimestamp(ts, tz=timezone.utc).strftime("%B %d, %Y %H:%M:%S UTC") if ts else None
 
-filtered_posts = [
-    {
-        "id": post.get("id"),
-        "subreddit": post.get("subreddit"),
-        "title": post.get("title"),
-        "selftext": post.get("selftext"),
-        "date": format_date(post.get("created_utc")),
-        "ups": post.get("ups"),
-        "downs": post.get("downs"),
-    }
-    for post in data if post_matches(post)
-    and "[deleted]" not in post.get("title", "").lower()
-    and "[deleted]" not in post.get("selftext", "").lower()
-    and "removed" not in post.get("title", "").lower()
-    and "removed" not in post.get("selftext", "").lower()
-    and post.get("title", "").strip() != ""
-    and post.get("selftext", "").strip() != ""
-]
-
 
 #ex: if path.name is r_CryptoScams_2020-2025_posts.jsonl, filename will be CryptoScams
 filename = path.name.split(".")[0].split('_')[1]
@@ -56,5 +38,24 @@ filename = path.name.split(".")[0].split('_')[1]
 output_dir = Path(__file__).resolve().parent.parent / filename / "data"  
 output_dir.mkdir(parents=True, exist_ok=True)
 with open(output_dir / f"filtered_{path.name}", "w", encoding="utf-8") as f:
-    for post in filtered_posts:
-        f.write(json.dumps(post) + "\n")
+    posts = extract_json_line(path)
+    for post in posts:
+        if (
+            post_matches(post)
+            and "[deleted]" not in post.get("title", "").lower()
+            and "[deleted]" not in post.get("selftext", "").lower()
+            and "removed" not in post.get("title", "").lower()
+            and "removed" not in post.get("selftext", "").lower()
+            and post.get("title", "").strip() != ""
+            and post.get("selftext", "").strip() != ""
+            ):
+            filtered_post = {
+                "id": post.get("id"),
+                "subreddit": post.get("subreddit"),
+                "title": post.get("title"),
+                "selftext": post.get("selftext"),
+                "date": format_date(post.get("created_utc")),
+                "ups": post.get("ups"),
+                "downs": post.get("downs"),
+            }
+            f.write(json.dumps(filtered_post) + "\n")
