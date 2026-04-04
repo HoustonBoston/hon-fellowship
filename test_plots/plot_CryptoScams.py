88dc6add16f68548b360c8faaa6d0eb14e476e4e
HoustonBoston <myhorsefly12345@gmail.com>
2026-04-04 10:02:26 -0400

    Added code to plot x = year, y = ups for test

--- test_plots/plot_CryptoScams.py
+++ test_plots/plot_CryptoScams.py
@@ -7,19 +7,24 @@ import pandas as pd
 
 # Convert to DataFrame
 df = pd.read_json("C:\\Users\\myhor\\projects\\hon-fellowship\\CryptoScams\\data\\filtered_r_CryptoScams_2020-2025_posts.jsonl", lines=True)
-df["year"] = pd.to_datetime(df["date"], errors="coerce").dt.year  # Convert date to year, handle errors
-df = df.dropna(subset=["year"])  # Drop rows where date conversion failed
 
-# Group by year and sum of ups for that year
-df = df.groupby("year")["ups"].sum()
+def plot_yearx_county(df):
+    df["year"] = pd.to_datetime(df["date"], errors="coerce").dt.year  # Convert date to year, handle errors
+    df = df.dropna(subset=["year"])  # Drop rows where date conversion failed
 
-# Print the first few rows to verify the data
-print(df.head())
+    # Group by year and sum of ups for that year
+    df = df.groupby("year")["ups"].sum()
 
-# Plot configs
-df.plot(x="year", y="ups", kind="bar", title="Ups over Time for Crypto Scams Posts")
-plt.xlabel("Year")
-plt.ylabel("Ups")
-plt.xticks(rotation=45)
+    # Print the first few rows to verify the data
+    print(df.head())
 
-plt.show()
+    # Plot configs
+    df.plot(x="year", y="ups", kind="bar", title="Ups over Time for Crypto Scams Posts")
+    plt.xlabel("Year")
+    plt.ylabel("Ups")
+    plt.xticks(rotation=45)
+
+    plt.show()
+
+if __name__ == "__main__":
+    plot_yearx_county(df)
